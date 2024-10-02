d0e2bd4965ce4d57b7d615750089bffb625484c7
Catherine Joyce <cannejoyce@outlook.com>
2024-10-02 19:56:49 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,7 +6,7 @@ repodir=/home/ec2-user/${reponame}
 
 # Install dependencies
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
+. ~/.nvm/nvm.sh
 nvm install 18
 nvm use 18
 cd $repodir
