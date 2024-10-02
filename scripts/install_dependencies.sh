f302a4480708fc51c33f310995a195daae75644f
Catherine Joyce <cannejoyce@outlook.com>
2024-10-02 20:42:30 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,8 +6,9 @@ repodir=/home/ec2-user/${reponame}
 
 # Install dependencies
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm" 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[d-7ZUB2MWI6][stdout][ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
 source ~/.bashrc
 yum install npm
 nvm install node
