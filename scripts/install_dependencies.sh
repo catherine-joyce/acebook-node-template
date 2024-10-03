d3090036fa0ba20932603a88f6249651bce1b468
Catherine Joyce <cannejoyce@outlook.com>
2024-10-03 09:32:02 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,10 +4,11 @@
 reponame=catherinej-app
 repodir=/home/ec2-user/${reponame}
 
-sudo chmod -R 777 /home/ec2-user/catherinej-app
+sudo chown -R ec2-user:ec2-user $repodir
+sudo chmod 777 $repodir
 
 # Navigate into our working directory
-cd /home/ec2-user/catherinej-app
+cd $repodir
 
 # add npm and node to path
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -15,17 +16,15 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 source ~/.bashrc
-yum install npm
-nvm install node
-nvm use node
-npm install
-cd $repodir
-npm i nodemon
+sudo yum install npm
+sudo nvm install node
+sudo nvm use node
+sudo npm install
+sudo npm i nodemon
 cd mongodb_install
-mv mongodb-org-8.0.repo /etc/yum.repos.d/
+sudo mv mongodb-org-8.0.repo /etc/yum.repos.d/
 cd ..
-yum install -y mongodb-org
+sudo yum install -y mongodb-org
 cd $repodir
-npm i nodemon
-systemctl start mongod
-(npm run start&)
+sudo npm i nodemon
+sudo systemctl start mongod
